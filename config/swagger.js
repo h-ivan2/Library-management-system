d0732ee5ec68c6af5eef63b53aeea19262c74f2a
h-ivan2 <hirwaivan884@gmail.com>
2026-02-09 20:51:39 +0200

    Initial commit

---
+++ config/swagger.js
@@ -0,0 +1,34 @@
+const swaggerJSDoc = require("swagger-jsdoc");
+
+const swaggerDefinition = {
+  openapi: "3.0.0",
+  info: {
+    title: "Library Management API",
+    version: "1.0.0",
+    description: "API documentation for Library Management System"
+  },
+  servers: [
+    {
+      url: "http://localhost:5000/api/v1",
+      description: "Development server"
+    }
+  ],
+  components: {
+    securitySchemes: {
+      bearerAuth: {
+        type: "http",
+        scheme: "bearer",
+        bearerFormat: "JWT"
+      }
+    }
+  }
+};
+
+const options = {
+  swaggerDefinition,
+  apis: ["./routes/*.js"]
+};
+
+const swaggerSpec = swaggerJSDoc(options);
+
+module.exports = swaggerSpec;
