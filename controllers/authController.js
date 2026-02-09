723a70008922598c05775c0b18489dd7be465c5b
h-ivan2 <hirwaivan884@gmail.com>
2026-02-09 20:51:39 +0200

    Initial commit

---
+++ controllers/authController.js
@@ -0,0 +1,72 @@
+const User = require("../models/User");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+
+const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    
+    if (!email || !password) {
+      return res.status(400).json({
+        message: "Email and password are required"
+      });
+    }
+
+    
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(400).json({
+        message: "Invalid email or password"
+      });
+    }
+
+    
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return res.status(400).json({
+        message: "Invalid email or password"
+      });
+    }
+
+    
+    const token = jwt.sign(
+      { id: user._id, role: user.role },
+      process.env.JWT_SECRET,
+      { expiresIn: "1h" }
+    );
+
+    res.json({
+      message: "Login successful",
+      token,
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+        role: user.role
+      }
+    });
+
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+const logoutUser=async(req,res)=>{
+     try{
+      res.json({message:"Logout sucessful"});
+}catch(error){
+  console.error(error);
+  res.status(500).json({message:'Server error'});
+
+}
+};
+
+module.exports = {
+  loginUser,
+  logoutUser
+};
