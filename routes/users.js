d84a68df9f481404a8a81776628630eae6c2bf74
h-ivan2 <hirwaivan884@gmail.com>
2026-02-11 09:22:54 +0200

    Added routes users.js file

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const { createUser, getAllUsers, deleteUser, updateUser } = require("../controllers/userController");
 
+//swagger comments
 /**
  * @swagger
  * /users:
