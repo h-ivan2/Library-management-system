0cabc080b677998dbf083bc879217b0360d40da0
h-ivan2 <hirwaivan884@gmail.com>
2026-02-21 18:32:10 +0200

    Updated seed.js so that it works with nodemon

--- scripts/seed.js
+++ scripts/seed.js
@@ -1,4 +1,4 @@
-require('dotenv').config({path: '../.env'});
+require('dotenv').config({path: require('path').resolve(__dirname, '../.env')});
 const mongoose=require('mongoose');
 const User=require('../models/User');
 const bcrypt=require('bcrypt')
