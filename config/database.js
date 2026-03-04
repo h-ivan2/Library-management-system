1e25a6884ba845b00ab04c750384b18b1245b1bb
h-ivan2 <hirwaivan884@gmail.com>
2026-03-04 18:48:31 +0200

    Made some configurations so that when an error occurs while connecting to database I can see it or when the database is disconnected

--- config/database.js
+++ config/database.js
@@ -2,10 +2,19 @@ const mongoose=require("mongoose");
 
 const connectDB =async() =>{
     try{
-        await mongoose.connect(process.env.MONGODB_URI);
-        console.log("MongoDB atlas connected");
+        
+        const conn=await mongoose.connect(process.env.MONGODB_URI);
+        console.log(`MongoDB atlas connected: ${conn.connection.host}`);
+
+        mongoose.connection.on('error',(err)=>{
+            console.error(`MongoDB connection error:${err}`);
+        });
+        
+        mongoose.connection.on('disconnected', () =>{
+            console.log('MongoDB disconnected');
+        })
     }catch(err){
-        console.error(err);
+        console.error(`Error: ${err.message}`);
         process.exit(1);
     }
 };
