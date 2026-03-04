8278ee052169cee3756683167ed60099a4bac249
h-ivan2 <hirwaivan884@gmail.com>
2026-03-04 18:56:11 +0200

    Added some due date functionality and fining so that user can be fined when exceeds the due date

--- models/borrow.js
+++ models/borrow.js
@@ -15,11 +15,48 @@ const borrowSchema = new mongoose.Schema({
     type: Date,
     default: Date.now
   },
+   due_date: {
+    type: Date,
+    required: true,
+    default: function() {
+      const date = new Date();
+      date.setDate(date.getDate() + 14);
+      return date;
+    }
+  },
   returned_at: {
     type: Date,
     default: null
+  },
+  fine: {
+    type: Number,
+    default: 0
+  },
+  
+  lastOverdueNotification:{
+    type:Date,
+    default:null
   }
 });
+
+borrowSchema.index({ userId: 1, returned_at: 1 });
+borrowSchema.index({ due_date: 1 });
+
+// checking if book is overdue
+borrowSchema.virtual('isOverdue').get(function() {
+  return !this.returned_at && this.due_date < new Date();
+});
+
+//if overdue we fine the borrower
+borrowSchema.methods.calculateFine = function() {
+  if (this.returned_at) return 0;
+  
+  const today = new Date();
+  if (this.due_date > today) return 0;
+  
+  const daysOverdue = Math.ceil((today - this.due_date) / (1000 * 60 * 60 * 24));
+  return daysOverdue * 1; // $1 per day fine
+};
 
 const Borrow=mongoose.model("Borrow",borrowSchema);
 module.exports =Borrow;
