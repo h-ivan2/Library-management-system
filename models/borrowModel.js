43f95509d22b267cc97461a1f598cc83e1ea6eae
hirwaivan <hirwaivan884@gmail.com>
2026-03-07 13:00:00 +0200

    chore: add due date field to borrow model

--- models/borrowModel.js
+++ models/borrowModel.js
@@ -1,3 +1,4 @@
 <!-- chore: update borrow model schema -->
 <!-- chore: add due date field to borrow model -->
 <!-- chore: update borrow model schema -->
+<!-- chore: add due date field to borrow model -->
