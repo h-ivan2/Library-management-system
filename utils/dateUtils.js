dae615ac7fb23ea909877140901117f855fda51c
hirwaivan <hirwaivan884@gmail.com>
2026-03-07 10:30:00 +0200

    fix: correct due date timezone handling

--- utils/dateUtils.js
+++ utils/dateUtils.js
@@ -2,3 +2,4 @@
 <!-- fix: correct due date timezone handling -->
 <!-- refactor: simplify date util exports -->
 <!-- feat: add due date calculation on borrow -->
+<!-- fix: correct due date timezone handling -->
