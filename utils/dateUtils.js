d37fad5193d4a7817080349cea32117010c1269e
hirwaivan <hirwaivan884@gmail.com>
2026-03-07 15:30:00 +0200

    refactor: simplify date util exports

--- utils/dateUtils.js
+++ utils/dateUtils.js
@@ -3,3 +3,4 @@
 <!-- refactor: simplify date util exports -->
 <!-- feat: add due date calculation on borrow -->
 <!-- fix: correct due date timezone handling -->
+<!-- refactor: simplify date util exports -->
