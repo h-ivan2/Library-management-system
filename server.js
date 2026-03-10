49378f2821f52b440e151a58d39d2e8d32612614
hirwaivan <hirwaivan884@gmail.com>
2026-03-10 13:00:00 +0200

    chore: register job in server.js

--- server.js
+++ server.js
@@ -58,3 +58,4 @@ app.listen(PORT, () => console.log(`Server running on port ${PORT}`));<!-- refac
 <!-- chore: register logger middleware in server.js -->
 <!-- chore: register health route in server.js -->
 <!-- refactor: clean up server.js imports -->
+<!-- chore: register job in server.js -->
