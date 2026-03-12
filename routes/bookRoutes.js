78f905c0c8f01117025d43aa298db00431f21d4c
hirwaivan <hirwaivan884@gmail.com>
2026-03-12 13:30:00 +0200

    refactor: merge title and author search into one route

--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -11,3 +11,4 @@
 <!-- feat: add pagination to book listing route -->
 <!-- feat: add search books by title endpoint -->
 <!-- feat: add search books by author endpoint -->
+<!-- refactor: merge title and author search into one route -->
