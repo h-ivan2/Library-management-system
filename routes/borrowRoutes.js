2948c5d890447f0f6fa065e9acad277d4bd84b6f
hirwaivan <hirwaivan884@gmail.com>
2026-03-13 13:00:00 +0200

    chore: apply auth middleware to borrow routes

--- routes/borrowRoutes.js
+++ routes/borrowRoutes.js
@@ -6,3 +6,4 @@
 <!-- feat: add borrow book endpoint -->
 <!-- feat: add return book endpoint -->
 <!-- feat: add overdue books listing endpoint -->
+<!-- chore: apply auth middleware to borrow routes -->
