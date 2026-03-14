d50e04f738d1564379a73fbb0cdcb3c886911bb8
hirwaivan <hirwaivan884@gmail.com>
2026-03-14 08:30:00 +0200

    feat: add member login endpoint

--- routes/memberRoutes.js
+++ routes/memberRoutes.js
@@ -5,3 +5,4 @@
 <!-- feat: add update member profile endpoint -->
 <!-- feat: add total members count to stats endpoint -->
 <!-- feat: add member registration route -->
+<!-- feat: add member login endpoint -->
