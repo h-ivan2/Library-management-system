27128c2c02c2907deecac156e9859825ac8c15f5
hirwaivan <hirwaivan884@gmail.com>
2026-03-17 08:30:00 +0200

    feat: add update member profile endpoint

--- routes/memberRoutes.js
+++ routes/memberRoutes.js
@@ -8,3 +8,4 @@
 <!-- feat: add member login endpoint -->
 <!-- feat: add get member profile endpoint -->
 <!-- fix: protect profile route with auth middleware -->
+<!-- feat: add update member profile endpoint -->
