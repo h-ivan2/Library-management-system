0b77078ddf11f38b4d62d8338c7abf74275ed07f
hirwaivan <hirwaivan884@gmail.com>
2026-03-20 21:40:25 +0200

    Final changes for deployment

--- middleware/rateLimiter.js
+++ middleware/rateLimiter.js
@@ -1,23 +1,24 @@
-const rateLimit=require('express-rate-limit');
+const rateLimit = require('express-rate-limit');
 
-const limiter=rateLimit({
-
-    windowMs: 1* 60 * 1000, //limit is 1 minute
-    max:50,//Limit each IP to 50 requests per minute
-    message:'Too many requests from this IP ,please try again later' ,
-    standardHeaders:true,
-    legacyHeaders:false,
+const limiter = rateLimit({
+    windowMs: 1 * 60 * 1000,
+    max: 50,
+    message: 'Too many requests from this IP, please try again later',
+    standardHeaders: true,
+    legacyHeaders: false,
+    validate: { xForwardedForHeader: false }
 });
 
-const loginLimiter=rateLimit({
-windowMs:15 *60 *1000,
-max:10 ,
-message:'Too manty login attempts , please try again later .',
-standardHeaders:true,
-legacyHeaders:false,
+const loginLimiter = rateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 10,
+    message: 'Too many login attempts, please try again later.',
+    standardHeaders: true,
+    legacyHeaders: false,
+    validate: { xForwardedForHeader: false }
 });
 
-module.exports={
+module.exports = {
     limiter,
     loginLimiter
 };
