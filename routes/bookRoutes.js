acfe4ee642ad9bfbb835cf940ffe8a46cc23c2e3
hirwaivan <hirwaivan884@gmail.com>
2026-03-21 10:30:00 +0200

    feat: add filter books by category endpoint

--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -4,3 +4,4 @@
 <!-- refactor: merge title and author search into one route -->
 <!-- feat: add delete book endpoint -->
 <!-- feat: add update book details endpoint -->
+<!-- feat: add filter books by category endpoint -->
