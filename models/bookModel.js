e17c237024d4ff1e0b97f3cf7d756c5e7261a46a
hirwaivan <hirwaivan884@gmail.com>
2026-03-22 08:30:00 +0200

    feat: add book rating field to book model

--- models/bookModel.js
+++ models/bookModel.js
@@ -2,3 +2,4 @@
 <!-- feat: add book rating field to book model -->
 <!-- feat: add book stock count field to book model -->
 <!-- feat: add book category field to book model -->
+<!-- feat: add book rating field to book model -->
