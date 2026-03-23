e5faed34b3cab185a663e495cbe32cfc8df966af
hirwaivan <hirwaivan884@gmail.com>
2026-03-23 13:30:00 +0200

    refactor: extract rating aggregation to utils

--- utils/bookUtils.js
+++ utils/bookUtils.js
@@ -1,3 +1,4 @@
 <!-- refactor: extract availability check to utils -->
 <!-- feat: add overdue detection logic -->
 <!-- refactor: move active borrow check to utils -->
+<!-- refactor: extract rating aggregation to utils -->
