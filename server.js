ec9a4067631e81f7639a09bce1ef67b0afb388e8
hirwaivan <hirwaivan884@gmail.com>
2026-03-26 10:30:00 +0200

    chore: register logger middleware in server.js

--- server.js
+++ server.js
@@ -55,3 +55,4 @@ app.use(errorMiddleware);
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));<!-- refactor: clean up server.js imports -->
 <!-- chore: register job in server.js -->
+<!-- chore: register logger middleware in server.js -->
