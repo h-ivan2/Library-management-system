9da8b396cb293a22c8a4a0354e6ed562d9c01e06
hirwaivan <hirwaivan884@gmail.com>
2026-04-11 08:30:00 +0200

    feat: add book review endpoint

--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -1,3 +1,4 @@
 <!-- feat: add book reservation endpoint -->
 <!-- feat: add cancel reservation endpoint -->
 <!-- feat: add get all reservations endpoint for admin -->
+<!-- feat: add book review endpoint -->
