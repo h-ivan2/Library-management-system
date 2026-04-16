2012edd9d0b4288d4b19511495531d42fba39447
hirwaivan <hirwaivan884@gmail.com>
2026-04-16 08:30:00 +0200

    feat: add refresh token endpoint

--- routes/memberRoutes.js
+++ routes/memberRoutes.js
@@ -1,3 +1,4 @@
 <!-- feat: add member wishlist endpoint -->
 <!-- feat: add remove from wishlist endpoint -->
 <!-- chore: apply rate limiter to auth routes -->
+<!-- feat: add refresh token endpoint -->
